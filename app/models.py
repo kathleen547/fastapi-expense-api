24168897b0261ff9d63cd11d37bf9d64056ae38b
kathleen547 <blueberry8947@gmail.com>
2026-05-18 17:56:15 +0200

    Create database models and setup

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, Float, Date, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+class Category(Base):
+    __tablename__= "category"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False, unique=True)
+    description = Column(String, nullable=True)
+
+    expenses = relationship("Expense", back_populates = "category")
+
+
+class Expense(Base):
+    __tablename__= "expense"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    amount = Column(Float, nullable=False)
+    date = Column(Date, nullable=False)
+    category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
+    category = relationship("Category", back_populates = "expenses")
+    notes = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), default=func.now())
+
